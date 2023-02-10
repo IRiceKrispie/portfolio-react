f8148668b23233416629fcc38358d88fc63ed96f
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-10 00:30:48 -0600

    fixed issue with a tag resizing img

--- portfolio-app/src/components/pages/Projects.js
+++ portfolio-app/src/components/pages/Projects.js
@@ -7,23 +7,13 @@ export default function Projects() {
   return (
     <div class="cards-list">
       <div class="card 1">
-        <div class="card_image">
-          <img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="Card 1" />
-        </div>
-        <div class="card_title title-white">
-          <p>Card Title</p>
-        </div>
-      </div>
-
-      <div class="card 2">
-        <div class="card_image">
-          <img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="Card 2"/>
+        <div class="card_image"> 
+          <a href="https://janvierb1.github.io/The-Movie-Library/"><img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="Card 1"/></a> 
         </div>
         <div class="card_title title-white">
           <p>Card Title</p>
         </div>
       </div>
     </div>
-    
   );
 }
