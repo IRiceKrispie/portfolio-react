433344b1b98d69dac99ccb8c51f9e4f464920777
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-09 23:04:49 -0600

    started projects page

--- portfolio-app/src/components/PortfolioContainer.js
+++ portfolio-app/src/components/PortfolioContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Footer from './Footer';
 import Header from './Header';
@@ -18,10 +18,13 @@ export default function PortfolioContainer() {
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Projects') {
+      return <Projects />;
     }
-    return <Contact />;
+    if (currentPage === 'Contact'){
+      return <Contact />;  
+    }
+    
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
