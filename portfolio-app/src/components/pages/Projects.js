11d0dac89867e98b8cc370cbcba4d2c322d7ceec
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-10 00:03:28 -0600

    added animations for cards

--- portfolio-app/src/components/pages/Projects.js
+++ portfolio-app/src/components/pages/Projects.js
@@ -8,19 +8,19 @@ export default function Projects() {
     <div class="cards-list">
       <div class="card 1">
         <div class="card_image">
-          <img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="Card 1" />
+          <img src="https://media.giphy.com/media/nNUUhTml69NLV3ksaR/giphy.gif" alt="Card 1" />
         </div>
-        <div class="card_title title-white">
-          <p>Card Title</p>
+        <div class="card_title title-black">
+          <p>Moive DB</p>
         </div>
       </div>
 
       <div class="card 2">
         <div class="card_image">
-          <img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="Card 2"/>
+          <img src="https://media.giphy.com/media/ekY8JdlX7FqQTbteE7/giphy.gif" alt="Card 2"/>
         </div>
-        <div class="card_title title-white">
-          <p>Card Title</p>
+        <div class="card_title title-black">
+          <p>Event Handler</p>
         </div>
       </div>
     </div>
